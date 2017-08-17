dfa05afcc25726816d073c438787932c18be6d9e
Juan Lera Diaz <jlera@hotmail.com>
2017-08-17 09:37:21 -0300

    Actualiza Google Calendar

--- src/pages/calendar/calendar.ts
+++ src/pages/calendar/calendar.ts
@@ -21,14 +21,15 @@ export class CalendarPage {
       email:""
     }];
   validation:any = {};
-  CLIENT_ID = '834168230719-tcrp9gh084um0g9th8vlk9l7svdu9oel.apps.googleusercontent.com'; // Client ID of your google console project
+  CLIENT_ID = '834168230719-tp6neuloqu901bbi16nfbmhckdmh1av9.apps.googleusercontent.com'; // Client ID of your google console project
   SCOPES = ["https://www.googleapis.com/auth/calendar"];
   APIKEY = "AIzaSyB8NDk0ltMFvrPZvFRo8CGabBhlwpwt1x8"; // API key of your google console project
   REDIRECTURL = "http://localhost/callback";
+  
+    browserRef: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
-              public inAppBrowser: InAppBrowser,
-              public inAppBrowserEvent: InAppBrowserEvent ) {
+              public inAppBrowser: InAppBrowser ) {
   }
 
   ionViewDidLoad() {
@@ -44,62 +45,119 @@ export class CalendarPage {
       var startDateTimeISO = this.buildISODate(this.calendarEvent.startDate, this.calendarEvent.startTime);
       var enddateTimeISO = this.buildISODate(this.calendarEvent.endDate, this.calendarEvent.endTime);
       this.popLastAttendeeIfEmpty(this.attendees);
-      var browserRef = InAppBrowser.call('https://accounts.google.com/o/oauth2/auth?client_id=' + this.CLIENT_ID + '&redirect_uri=' + this.REDIRECTURL + '&scope=https://www.googleapis.com/auth/calendar&approval_prompt=force&response_type=token', '_blank', 'location=no');         
-      browserRef.addEventListener("loadstart", (event) => {
-        if ((event["url"]).indexOf("http://localhost/callback") === 0) {
-          var url = event["url"];
-          var token = url.split('access_token=')[1].split('&token_type')[0];
-          browserRef.removeEventListener("exit", (event) => { });
-          browserRef.close();
-          //SENDING THE INVITE USING THE GOOGLE CALENDAR API
-          gapi.client.setApiKey(this.APIKEY);
-          Request = gapi.client.request({
-            'path': '/calendar/v3/calendars/primary/events?alt=json',
-            'method': 'POST',
-            'headers': {
-              'Authorization': 'Bearer ' + token
-            },
-            'body': JSON.stringify({
-              "summary": this.calendarEvent.name,
-              "location": this.calendarEvent.location,
-              "description": this.calendarEvent.description,
-              "start": {
-                "dateTime": startDateTimeISO,
-                "timeZone": "Asia/Kolkata" // TODO : Parameterize this
-              },
-              "end": {
-                "dateTime": enddateTimeISO,
-                "timeZone": "Asia/Kolkata" // TODO : Parameterize this
-              },
-              "recurrence": [
-                "RRULE:FREQ=DAILY;COUNT=1" //// TODO : Parameterize this, Frequency of the event
-              ],
-              "attendees": this.attendees,
-              "reminders": {
-                "useDefault": false,
-                "overrides": [
-                  {
-                    "method": "email",
-                    "minutes": 1440   		// TODO : Parameterize this, No. of minutes before you want google services to send an email reminder
+      let browserRef = this.inAppBrowser.create('https://accounts.google.com/o/oauth2/auth?client_id=' + this.CLIENT_ID + '&redirect_uri=' + this.REDIRECTURL + '&scope=https://www.googleapis.com/auth/calendar&approval_prompt=force&response_type=token', '_blank', 'location=no');         
+      
+      browserRef.on('loadstart').subscribe((event: InAppBrowserEvent) => {
+          if (event.url.indexOf('http://localhost:8100') === 0) {
+            var url = event["url"];
+            var token = url.split('access_token=')[1].split('&token_type')[0];
+            browserRef.on('exit').subscribe((event) => { });
+            browserRef.close();
+            //SENDING THE INVITE USING THE GOOGLE CALENDAR API
+            gapi.client.setApiKey(this.APIKEY);
+            Request = gapi.client.request({
+                'path': '/calendar/v3/calendars/primary/events?alt=json',
+                'method': 'POST',
+                'headers': {
+                  'Authorization': 'Bearer ' + token
+                },
+                'body': JSON.stringify({
+                  "summary": this.calendarEvent.name,
+                  "location": this.calendarEvent.location,
+                  "description": this.calendarEvent.description,
+                  "start": {
+                    "dateTime": startDateTimeISO,
+                    "timeZone": "Asia/Kolkata" // TODO : Parameterize this
                   },
-                  {
-                    "method": "popup",
-                    "minutes": 10 				// TODO : Parameterize this, No. of minutes before you want google services to send an popup reminder
+                  "end": {
+                    "dateTime": enddateTimeISO,
+                    "timeZone": "Asia/Kolkata" // TODO : Parameterize this
+                  },
+                  "recurrence": [
+                    "RRULE:FREQ=DAILY;COUNT=1" //// TODO : Parameterize this, Frequency of the event
+                  ],
+                  "attendees": this.attendees,
+                  "reminders": {
+                    "useDefault": false,
+                    "overrides": [
+                      {
+                        "method": "email",
+                        "minutes": 1440   		// TODO : Parameterize this, No. of minutes before you want google services to send an email reminder
+                      },
+                      {
+                        "method": "popup",
+                        "minutes": 10 				// TODO : Parameterize this, No. of minutes before you want google services to send an popup reminder
+                      }
+                    ]
                   }
-                ]
-              }
-            }),
-            'callback': function (jsonR, rawR) {
-              if(jsonR.id){
-                alert("Invitation sent successfully");
-              } else {
-                alert("Failed to sent invite.");
-              }
-              console.log(jsonR); // Everything related to invite once created, use this for further enhancements
-            }
-          });
-        }
+                }),
+                'callback': function (jsonR, rawR) {
+                  if(jsonR.id){
+                    alert("Invitation sent successfully");
+                  } else {
+                    alert("Failed to sent invite.");
+                  }
+                  console.log(jsonR); // Everything related to invite once created, use this for further enhancements
+                }
+            });
+          }
       });
+
+      // browserRef.addEventListener("loadstart", (event) => {
+      //   if ((event["url"]).indexOf("http://localhost/callback") === 0) {
+      //     var url = event["url"];
+      //     var token = url.split('access_token=')[1].split('&token_type')[0];
+      //     browserRef.removeEventListener("exit", (event) => { });
+      //     browserRef.close();
+      //     //SENDING THE INVITE USING THE GOOGLE CALENDAR API
+      //     gapi.client.setApiKey(this.APIKEY);
+      //     Request = gapi.client.request({
+      //       'path': '/calendar/v3/calendars/primary/events?alt=json',
+      //       'method': 'POST',
+      //       'headers': {
+      //         'Authorization': 'Bearer ' + token
+      //       },
+      //       'body': JSON.stringify({
+      //         "summary": this.calendarEvent.name,
+      //         "location": this.calendarEvent.location,
+      //         "description": this.calendarEvent.description,
+      //         "start": {
+      //           "dateTime": startDateTimeISO,
+      //           "timeZone": "Asia/Kolkata" // TODO : Parameterize this
+      //         },
+      //         "end": {
+      //           "dateTime": enddateTimeISO,
+      //           "timeZone": "Asia/Kolkata" // TODO : Parameterize this
+      //         },
+      //         "recurrence": [
+      //           "RRULE:FREQ=DAILY;COUNT=1" //// TODO : Parameterize this, Frequency of the event
+      //         ],
+      //         "attendees": this.attendees,
+      //         "reminders": {
+      //           "useDefault": false,
+      //           "overrides": [
+      //             {
+      //               "method": "email",
+      //               "minutes": 1440   		// TODO : Parameterize this, No. of minutes before you want google services to send an email reminder
+      //             },
+      //             {
+      //               "method": "popup",
+      //               "minutes": 10 				// TODO : Parameterize this, No. of minutes before you want google services to send an popup reminder
+      //             }
+      //           ]
+      //         }
+      //       }),
+      //       'callback': function (jsonR, rawR) {
+      //         if(jsonR.id){
+      //           alert("Invitation sent successfully");
+      //         } else {
+      //           alert("Failed to sent invite.");
+      //         }
+      //         console.log(jsonR); // Everything related to invite once created, use this for further enhancements
+      //       }
+      //     });
+      //   }
+      // });
   }
   buildISODate(date, time){
       var dateArray = date && date.split('-');
